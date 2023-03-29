a2f733360c8f3a13896182febf37920c78ddccc2
Cassandra Ganska <89425242+CasGan@users.noreply.github.com>
2023-03-29 08:42:32 -0600

    Update gen.py

--- Modules/keygen/controlflow2_1/gen.py
+++ Modules/keygen/controlflow2_1/gen.py
@@ -1,28 +1,24 @@
 import string
-# import random 
-# import numpy as np
-
 import secrets
 
 letters = string.ascii_letters
 digits = string.digits
-special_chars = string.punctuation
-alphabet = letters + digits + special_chars
 
+alphabet = letters + digits
 pwd_length = 16
 pwd = ''
 
 for i in range(pwd_length):
-    if i==0:
+    if i==6:
+        pwd += ''.join(secrets.choice('Y'))
+    elif i==8:
+        pwd += ''.join(secrets.choice('#'))
+    elif i==10:
         pwd += ''.join(secrets.choice('A'))
-    elif i==1:
-        pwd += ''.join(secrets.choice('6'))
-    elif i==3:
-        pwd += ''.join(secrets.choice('2'))
-    elif i==7:
-        pwd += ''.join(secrets.choice('%'))
-    elif i == 15:
+    elif i==11:
         pwd += ''.join(secrets.choice('*'))
+    elif i == 13:
+        pwd += ''.join(secrets.choice('6'))
     else:
         pwd += ''.join(secrets.choice(alphabet))
 
